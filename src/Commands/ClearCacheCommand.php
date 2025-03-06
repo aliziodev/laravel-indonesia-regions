ae654f4254f7c751c075640941e6e6e3be5d0444
MuHanz <muhanz21@gmail.com>
2025-03-07 05:11:56 +0700

    fix typo named

--- src/Commands/ClearCacheCommand.php
+++ src/Commands/ClearCacheCommand.php
@@ -3,18 +3,18 @@
 namespace Aliziodev\IndonesiaRegions\Commands;
 
 use Illuminate\Console\Command;
-use Aliziodev\IndonesiaRegions\Facades\Indonesian;
+use Aliziodev\IndonesiaRegions\Facades\Indonesia;
 
 class ClearCacheCommand extends Command
 {
-    protected $signature = 'indonesian-regions:clear-cache';
+    protected $signature = 'indonesia-regions:clear-cache';
     protected $description = 'Clear Indonesia Regions cache';
 
     public function handle()
     {
         $this->info('Clearing Indonesia Regions cache...');
 
-        if (Indonesian::clearCache()) {
+        if (Indonesia::clearCache()) {
             $this->info('Cache cleared successfully!');
         } else {
             $this->error('Failed to clear cache!');
