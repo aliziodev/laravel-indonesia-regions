11964dab9341e62f60804acb78cf0d0188cc79c3
MuHanz <muhanz21@gmail.com>
2025-03-07 05:23:02 +0700

    following better separation of concerns in model

--- src/Models/IndonesiaRegion.php
+++ src/Models/IndonesiaRegion.php
@@ -8,8 +8,8 @@ class IndonesiaRegion extends Model
 {
     protected $table = 'indonesia_regions';
     protected $primaryKey = 'code';
-    public $incrementing = false;
     protected $keyType = 'string';
+    public $incrementing = false;
     public $timestamps = false;
 
     protected $fillable = [
@@ -20,33 +20,4 @@ class IndonesiaRegion extends Model
         'longitude',
         'status'
     ];
-
-    public function isProvince(): bool
-    {
-        return strlen($this->code) === 2;
-    }
-
-    public function isCity(): bool
-    {
-        return strlen($this->code) === 5;
-    }
-
-    public function isDistrict(): bool
-    {
-        return strlen($this->code) === 8;
-    }
-
-    public function isVillage(): bool
-    {
-        return strlen($this->code) === 13;
-    }
-
-    public function getParentCode(): ?string
-    {
-        if ($this->isProvince()) return null;
-        if ($this->isCity()) return substr($this->code, 0, 2);
-        if ($this->isDistrict()) return substr($this->code, 0, 5);
-        if ($this->isVillage()) return substr($this->code, 0, 8);
-        return null;
-    }
 }
