93de67ca0f9023148facb0a38b8ab0b069ca4e96
Mu Hanz <muhanz21@gmail.com>
2026-03-09 01:52:02 +0700

    feat: Add API configuration options to indonesia-regions.php

--- config/indonesia-regions.php
+++ config/indonesia-regions.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'data_path' => env('INDONESIA_REGIONS_DATA_PATH', __DIR__.'/../data'),
+
+    'api' => [
+        'enabled' => env('INDONESIA_REGIONS_API_ENABLED', true),
+        'prefix' => env('INDONESIA_REGIONS_API_PREFIX', 'api/indonesia-regions'),
+        'middleware' => ['api'],
+        'responder' => null,
+    ],
+
     'cache' => [
         'store' => env('INDONESIA_REGIONS_CACHE_STORE'),
         'ttl' => (int) env('INDONESIA_REGIONS_CACHE_TTL', 86400),
